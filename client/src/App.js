be8b1e2ca148a99311e5878e5d253e20d7e626f5
Jonathan Ho <johnnyho82@gmail.com>
2019-11-03 16:16:48 -0500

    Update header

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import HelloLazy from './components/HelloLazy';
 function App() {
   return (
     <div className='App'>
-      <h1>React App using Apollo Client</h1>
+      <h1>React App - useLazyQuery</h1>
       <HelloLazy />
     </div>
   );
